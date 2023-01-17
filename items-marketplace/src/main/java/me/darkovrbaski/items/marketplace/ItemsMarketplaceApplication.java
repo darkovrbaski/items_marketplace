caa36b62986bb114a1dc15f554215c626c55ff76
darkovrbaski <vrbaski.ra200.2018@uns.ac.rs>
2023-01-17 18:53:26 +0100

    feat: initial commit

---
+++ items-marketplace/src/main/java/me/darkovrbaski/items/marketplace/ItemsMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package me.darkovrbaski.items.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsMarketplaceApplication.class, args);
+	}
+
+}
---
+++ items-marketplace/src/test/java/me/darkovrbaski/items/marketplace/ItemsMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.darkovrbaski.items.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
