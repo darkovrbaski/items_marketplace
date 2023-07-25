a8e250b0a78129751fbf3f3bb572b0e78b9d0180
darkovrbaski <vrbaski.ra200.2018@uns.ac.rs>
2023-07-25 17:18:55 +0200

    chore: changed backend url

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.darkovrbaski.me:8443',
+  apiUrl: 'https://api.darkovrbaski.me',
   stripe:
     'pk_test_51Mti9XFql4kOKatvUN1ztyVI5z9XbuG47BsADvbKOdWoKAU36bbuZmuxJsizBCPDu7fKAspXHT8zm3jjhZz6768T00UCnESmgz',
 };
